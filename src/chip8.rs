eaf4c7238dc9ae9407878e2c8a137b4bf56bf158
Mateo Gonzalez <magonzalezoc@gmail.com>
2021-04-06 13:52:54 -0500

    Finished CPU instructions, fixed some bugs and added more ROMS to test

--- src/chip8.rs
+++ src/chip8.rs
@@ -27,44 +27,69 @@ impl Emulator {
             let opcode = self.cpu.get_op(&self.memory);
             self.exec_code(opcode);
             self.screen.update();
+            self.cpu.update();
         }
         Ok(())
     }
 
     fn exec_code(&mut self, opcode: u16) {
-        let codes = self.parse_opcode(opcode);
+        //println!("{:#02x}", opcode);
         let addr = opcode & 0xFFF;
         let kk = (opcode & 0xFF) as u8;
-        match codes {
-            (0, 0, 0xE, 0) => self.screen.clear(),
-            (0, 0, 0xE, 0xE) => self.cpu.return_from_subroutine(),
-            (1, _, _, _) => self.cpu.jump(addr),
-            (2, _, _, _) => self.cpu.call(addr),
-            (3, x, _, _) => self.cpu.skip_equal(x, kk, false),
-            (4, x, _, _) => self.cpu.skip_not_equal(x, kk, false),
-            (5, x, y, 0) => self.cpu.skip_equal(x, y, true),
-            (6, x, _, _) => self.cpu.load(x, kk, false),
-            (7, x, _, _) => self.cpu.add(x, kk, false),
-            (8, x, y, 0) => self.cpu.add(x, y, true),
-            (8, x, y, 1) => self.cpu.or(x, y),
-            (8, x, y, 2) => self.cpu.and(x, y),
-            (8, x, y, 3) => self.cpu.xor(x, y),
-            (8, x, y, 4) => self.cpu.add_with_carrier(x, y),
-            (8, x, y, 5) => self.cpu.sub_with_carrier(x, y),
-            (8, x, _, 6) => self.cpu.shift_right(x),
-            (8, x, y, 7) => self.cpu.sub_not_borrow(x, y),
-            (8, x, _, 0xE) => self.cpu.shift_left(x),
-            (9, x, y, _) => self.cpu.skip_not_equal(x, y, true),
-            (_, _, _, _) => panic!("Unexpected code"),
+        let x = ((opcode >> 8) & 0xF) as u8;
+        let y = ((opcode >> 4) & 0xF) as u8;
+        let n = (opcode & 0xF) as u8;
+        let code_id = ((opcode >> 12) & 0xF) as u8;
+        match code_id {
+            0x0 => match n {
+                0x0 => self.cpu.clear_screen(&mut self.screen),
+                0xE => self.cpu.return_from_subroutine(),
+                _ => panic!(format!("Unexpected code {}", opcode)),
+            },
+            1 => self.cpu.jump(addr),
+            2 => self.cpu.call(addr),
+            3 => self.cpu.skip_equal(x, kk, false),
+            4 => self.cpu.skip_not_equal(x, kk, false),
+            5 => self.cpu.skip_equal(x, y, true),
+            6 => self.cpu.load(x, kk, false),
+            7 => self.cpu.add(x, kk, false),
+            8 => match n {
+                0 => self.cpu.load(x, y, true),
+                1 => self.cpu.or(x, y),
+                2 => self.cpu.and(x, y),
+                3 => self.cpu.xor(x, y),
+                4 => self.cpu.add_with_carrier(x, y),
+                5 => self.cpu.sub_with_carrier(x, y),
+                6 => self.cpu.shift_right(x),
+                7 => self.cpu.sub_not_borrow(x, y),
+                0xE => self.cpu.shift_left(x),
+                _ => panic!(format!("Unexpected code {}", opcode)),
+            },
+            9 => self.cpu.skip_not_equal(x, y, true),
+            0xA => self.cpu.load_i(addr),
+            0xB => self.cpu.jump_v0(addr),
+            0xC => self.cpu.set_random_byte(x, kk),
+            0xD => self
+                .cpu
+                .update_sprite(&self.memory, &mut self.screen.buffer, x, y, n),
+            0xE => match kk {
+                0x9E => self.cpu.skip_if_key_is_pressed(x, &self.screen),
+                0xA1 => self.cpu.skip_if_key_is_not_pressed(x, &self.screen),
+                _ => panic!(format!("Unexpected code {}", opcode)),
+            },
+            0xF => match kk {
+                0x07 => self.cpu.load_delay(x),
+                0x0A => self.cpu.wait_for_key_press(x, &self.screen),
+                0x15 => self.cpu.set_delay_timer(x),
+                0x18 => self.cpu.set_sound_timer(x),
+                0x1E => self.cpu.set_i(x),
+                0x29 => self.cpu.load_sprite(x),
+                0x33 => self.cpu.store_bcd(x, &mut self.memory),
+                0x55 => self.cpu.store_registers(x, &mut self.memory),
+                0x65 => self.cpu.load_registers(x, &mut self.memory),
+                _ => panic!(format!("Unexpected code {}", opcode)),
+            },
+            _ => panic!(format!("Unexpected code {}", opcode)),
         };
-    }
-
-    fn parse_opcode(&self, opcode: u16) -> (u8, u8, u8, u8) {
-        (
-            (opcode << 12) as u8,
-            ((opcode & 0xF00) >> 8) as u8,
-            ((opcode & 0xF0) >> 4) as u8,
-            (opcode & 0xF) as u8,
-        )
     }
 }
--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,8 @@
 use super::memory::Memory;
+use super::screen::{key_to_hex, Screen};
+use rand::prelude::*;
+
+const FG_COLOR: u32 = 0x61AFEF;
 
 #[derive(Debug)]
 pub struct CPU {
@@ -9,6 +13,7 @@ pub struct CPU {
     pc: u16,
     sp: u8,
     stack: [u16; 16],
+    rng: ThreadRng,
 }
 
 impl CPU {
@@ -21,6 +26,7 @@ impl CPU {
             pc: 0x200,
             sp: 0,
             stack: [0; 16],
+            rng: rand::thread_rng(),
         }
     }
 
@@ -28,9 +34,25 @@ impl CPU {
         ((memory.get(self.pc as usize) as u16) << 8) | memory.get((self.pc + 1) as usize) as u16
     }
 
+    pub fn update(&mut self) {
+        if self.delay > 0 {
+            self.delay -= 1;
+        }
+
+        if self.sound > 0 {
+            self.sound -= 1;
+        }
+    }
+
+    pub fn clear_screen(&mut self, screen: &mut Screen) {
+        screen.clear();
+        self.pc += 2;
+    }
+
     pub fn return_from_subroutine(&mut self) {
-        self.pc = self.stack[self.sp as usize];
         self.sp -= 1;
+        self.pc = self.stack[self.sp as usize];
+        self.pc += 2
     }
 
     pub fn jump(&mut self, addr: u16) {
@@ -38,8 +60,8 @@ impl CPU {
     }
 
     pub fn call(&mut self, addr: u16) {
-        self.sp += 1;
         self.stack[self.sp as usize] = self.pc;
+        self.sp += 1;
         self.pc = addr;
     }
 
@@ -124,24 +146,122 @@ impl CPU {
 
         self.v[0xF] = if !overflow { 1 } else { 0 };
         self.v[x as usize] = res;
+        self.pc += 2;
     }
 
     pub fn shift_right(&mut self, x: u8) {
-        self.v[0xF] = if (self.v[x as usize]) & 0b00000001 == 1 {
-            1
-        } else {
-            0
-        };
+        self.v[0xF] = self.v[x as usize] & 0b1;
         self.v[x as usize] >>= 1;
         self.pc += 2;
     }
     pub fn shift_left(&mut self, x: u8) {
-        self.v[0xF] = if ((self.v[x as usize] & 0b10000000) >> 7) == 1 {
-            1
-        } else {
-            0
-        };
+        self.v[0xF] = self.v[x as usize] >> 7;
         self.v[x as usize] <<= 1;
+        self.pc += 2;
+    }
+
+    pub fn load_i(&mut self, addr: u16) {
+        self.i = addr;
+        self.pc += 2;
+    }
+
+    pub fn jump_v0(&mut self, addr: u16) {
+        self.pc = self.v[0] as u16 + addr;
+    }
+
+    pub fn set_random_byte(&mut self, x: u8, kk: u8) {
+        let random_byte: u8 = self.rng.gen();
+        self.v[x as usize] = random_byte & kk;
+        self.pc += 2;
+    }
+
+    pub fn update_sprite(&mut self, memory: &Memory, buffer: &mut [u32], x: u8, y: u8, n: u8) {
+        self.v[0xF] = 0;
+        let vx = self.v[x as usize];
+        let vy = self.v[y as usize];
+        for offset in 0..n {
+            let value = memory.get((self.i + offset as u16) as usize);
+            let i = (vy as usize + offset as usize) % 32;
+            for bit in 0..8 {
+                let j = (vx as usize + bit) % 64;
+                let index = j + 64 * i;
+                let old_pixel = buffer[index];
+                let new_pixel = ((value >> (7 - bit)) & 0b1) as u32 * FG_COLOR;
+
+                buffer[index] ^= new_pixel;
+
+                if old_pixel == FG_COLOR && new_pixel == FG_COLOR {
+                    self.v[0xF] = 1;
+                }
+            }
+        }
+        self.pc += 2;
+    }
+
+    pub fn skip_if_key_is_pressed(&mut self, x: u8, screen: &Screen) {
+        self.skip_if_condition(screen.is_key_down(self.v[x as usize]));
+        self.pc += 2;
+    }
+
+    pub fn skip_if_key_is_not_pressed(&mut self, x: u8, screen: &Screen) {
+        self.skip_if_condition(!screen.is_key_down(self.v[x as usize]));
+        self.pc += 2;
+    }
+
+    pub fn load_delay(&mut self, x: u8) {
+        self.v[x as usize] = self.delay;
+        self.pc += 2;
+    }
+
+    pub fn wait_for_key_press(&mut self, x: u8, screen: &Screen) {
+        if let Some(keys) = screen.is_any_key_down() {
+            if keys.len() > 0 {
+                let key = keys[0];
+                self.v[x as usize] = key_to_hex(key);
+                self.pc += 2;
+            }
+        }
+    }
+
+    pub fn set_delay_timer(&mut self, x: u8) {
+        self.delay = self.v[x as usize];
+        self.pc += 2;
+    }
+
+    pub fn set_sound_timer(&mut self, x: u8) {
+        self.sound = self.v[x as usize];
+        self.pc += 2;
+    }
+
+    pub fn set_i(&mut self, x: u8) {
+        self.i += self.v[x as usize] as u16;
+        self.pc += 2;
+    }
+
+    pub fn load_sprite(&mut self, x: u8) {
+        self.i = self.v[x as usize] as u16 * 5;
+        self.pc += 2;
+    }
+
+    pub fn store_bcd(&mut self, x: u8, memory: &mut Memory) {
+        let vx = self.v[x as usize];
+        memory.set(self.i as usize, (vx / 100) % 10);
+        memory.set(self.i as usize + 1, (vx / 10) % 10);
+        memory.set(self.i as usize + 2, vx % 10);
+        self.pc += 2;
+    }
+
+    pub fn store_registers(&mut self, x: u8, memory: &mut Memory) {
+        for offset in 0..(x + 1) {
+            memory.set((self.i + offset as u16) as usize, self.v[offset as usize])
+        }
+        self.pc += 2;
+    }
+
+    pub fn load_registers(&mut self, x: u8, memory: &mut Memory) {
+        for offset in 0..(x + 1) {
+            self.v[offset as usize] = memory.get((self.i + offset as u16) as usize);
+        }
         self.pc += 2;
     }
 }
